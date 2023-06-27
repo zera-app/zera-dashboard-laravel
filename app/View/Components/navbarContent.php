5c12791beec1ec9cbcfadfac21ab23fa3a4001b5
JunedSetiawan <junedhayko502@gmail.com>
2023-06-27 20:52:44 +0700

    update readme

--- app/View/Components/navbarContent.php
+++ app/View/Components/navbarContent.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class navbar-content extends Component
+class navbarContent extends Component
 {
     /**
      * Create a new component instance.
--- app/View/Components/sidebarContent.php
+++ app/View/Components/sidebarContent.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class sidebar-content extends Component
+class sidebarContent extends Component
 {
     /**
      * Create a new component instance.
