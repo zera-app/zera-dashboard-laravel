b69940967815013caf10ffe2178592af414652c8
JunedSetiawan <junedhayko502@gmail.com>
2023-07-02 21:22:20 +0700

    fix error component splade

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,8 @@ import "@protonemedia/laravel-splade/dist/style.css";
 
 import ThemeToggle from "./components/ThemeToggle.vue";
 
+// import "ionicons/dist/ionicons.js";
+
 import { createApp } from "vue/dist/vue.esm-bundler.js";
 import { renderSpladeApp, SpladePlugin } from "@protonemedia/laravel-splade";
 
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,6 +1,6 @@
 <div class="drawer-side shadow-sm">
     <label for="my-drawer-2" class="drawer-overlay"></label>
-    <ul class="menu p-4 w-64 min-h-full bg-base-100 text-base-content overflow-y-auto">
+    <ul class="menu p-4 w-64 min-h-full bg-base-100 text-base-content overflow-y-auto space-y-1">
         <!-- Sidebar content here -->
         <x-sidebar-content />
     </ul>
