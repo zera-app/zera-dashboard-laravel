6a2b0f32904ed7b1b99f2f96f17319c5361b6222
JunedSetiawan <junedhayko502@gmail.com>
2023-07-07 20:59:52 +0700

    update dark mode for table

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,13 +2,9 @@
     <x-slot name="headerNav">
         {{ __("Dashboard") }}
     </x-slot>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-base-100 dark:bg-neutral overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 border-b border-base-100 text-base-content">
-                    You're logged in
-                </div>
-            </div>
-        </div>
-    </div>
+
+
+    You're logged in
+
+
 </x-app-layout>
