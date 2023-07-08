be9f732981a07e794032f294a7cdbb4ad8533d26
JunedSetiawan <junedhayko502@gmail.com>
2023-07-08 10:37:17 +0700

    update more dark mode for table

--- resources/views/vendor/splade/components/button-with-dropdown.blade.php
+++ resources/views/vendor/splade/components/button-with-dropdown.blade.php
@@ -1,9 +1,9 @@
-<x-splade-component is="dropdown" {{ $attributes->class('w-full bg-white border border-gray-300 rounded-md shadow-sm px-2.5 sm:px-4 py-2 inline-flex justify-center text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500') }}>
+<x-splade-component is="dropdown" {{ $attributes->class('w-full bg-base-100 border border-base-300 rounded-md shadow-sm px-2.5 sm:px-4 py-2 inline-flex justify-center text-sm font-medium text-base-content hover:bg-base-200 focus:outline-none focus:ring-1 focus:ring-secondary') }}>
     <x-slot:trigger>
         {{ $button }}
     </x-slot:trigger>
 
-    <div class="mt-2 min-w-max rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+    <div class="mt-2 min-w-max rounded-md shadow-lg bg-base-100 ring-1 ring-black ring-opacity-5">
         {{ $slot }}
     </div>
 </x-splade-component>
