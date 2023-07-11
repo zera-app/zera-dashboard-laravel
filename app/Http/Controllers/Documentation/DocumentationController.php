7e989c565aa8c7b555f7e066c7e43ac0118cacda
Zulfikar Ditya <zulfikarditya.work@gmail.com>
2023-07-11 10:40:37 +0700

    Add Controllers and views for documentations

--- app/Http/Controllers/Documentation/DocumentationController.php
+++ app/Http/Controllers/Documentation/DocumentationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Documentation;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class DocumentationController extends Controller
 {
@@ -14,60 +15,450 @@ class DocumentationController extends Controller
         return view('documentation.index');
     }
 
+    // ! HELPERS DOCUMENTATION ---------------------------------------------------------------
+
     /**
-     * Show the documentation for sidebar
+     * Documentation for controller helper.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function controllerHelper(): View
+    {
+        $this->spladeTitle('Controller Helper');
+
+        return view('documentation.controller-helper');
+    }
+
+    /**
+     * Documentation for upload file helper.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function uploadFileHelper()
+    {
+        $this->spladeTitle('Upload File Helper');
+
+        return view('documentation.upload-file-helper');
+    }
+
+    /**
+     * Documentation for splade helper.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function spladeHelper()
+    {
+        $this->spladeTitle('Splade Helper');
+
+        return view('documentation.splade-helper');
+    }
+
+    // ! / END HELPERS DOCUMENTATION ---------------------------------------------------------------
+
+    // ! PAGES DOCUMENTATION ---------------------------------------------------------------
+    /**
+     * Documentation for sidebar.
+     * 
+     * @return \Illuminate\View\View
      */
     public function sidebar()
     {
-        $this->spladeTitle('Sidebar Documentation');
+        $this->spladeTitle('Sidebar');
 
         return view('documentation.sidebar');
     }
 
     /**
-     * Show the documentation for card
+     * Documentation for config.
+     * 
+     * @return \Illuminate\View\View
      */
-    public function card()
+    public function config()
     {
-        return view('documentation.card');
+        $this->spladeTitle('Config');
+
+        return view('documentation.config');
+    }
+    // ! / END PAGES DOCUMENTATION ---------------------------------------------------------------
+
+    // ! COMPONENTS DOCUMENTATION ---------------------------------------------------------------
+    /**
+     * Documentation for alert.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function alert()
+    {
+        $this->spladeTitle('Alert');
+
+        return view('documentation.alert');
     }
 
     /**
-     * Show the documentation for button
+     * Documentation for badge.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function badge()
+    {
+        $this->spladeTitle('Badge');
+
+        return view('documentation.badge');
+    }
+
+    /**
+     * Documentation for breadcrumb.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function breadcrumb()
+    {
+        $this->spladeTitle('Breadcrumb');
+
+        return view('documentation.breadcrumb');
+    }
+
+    /**
+     * Documentation for button.
+     * 
+     * @return \Illuminate\View\View
      */
     public function button()
     {
+        $this->spladeTitle('Button');
+
         return view('documentation.button');
     }
 
     /**
-     * Show the documentation for input
+     * Documentation for card.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function card()
+    {
+        $this->spladeTitle('Card');
+
+        return view('documentation.card');
+    }
+
+    /**
+     * Documentation for collapse.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function collapse()
+    {
+        $this->spladeTitle('Collapse');
+
+        return view('documentation.collapse');
+    }
+
+    /**
+     * Documentation for modal.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function modal()
+    {
+        $this->spladeTitle('Modal');
+
+        return view('documentation.modal');
+    }
+
+    /**
+     * Documentation for tooltip.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function tooltip()
+    {
+        $this->spladeTitle('Tooltip');
+
+        return view('documentation.tooltip');
+    }
+    // ! / END COMPONENTS DOCUMENTATION ---------------------------------------------------------------
+
+    // ! SPLADE COMPONENT DOCUMENTATION ---------------------------------------------------------------
+    /**
+     * Documentation for splade flash.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function flash()
+    {
+        $this->spladeTitle('Flash');
+
+        return view('documentation.flash');
+    }
+
+    /**
+     * Documentation for splade form.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function form()
+    {
+        $this->spladeTitle('Form');
+
+        return view('documentation.form');
+    }
+
+    /**
+     * Documentation for splade link.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function link()
+    {
+        $this->spladeTitle('Link');
+
+        return view('documentation.link');
+    }
+
+    /**
+     * Documentation for splade script.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function script()
+    {
+        $this->spladeTitle('Script');
+
+        return view('documentation.script');
+    }
+
+    /**
+     * Documentation for splade toggle.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function toggle()
+    {
+        $this->spladeTitle('Toggle');
+
+        return view('documentation.toggle');
+    }
+
+    /**
+     * Documentation for splade transition.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function transition()
+    {
+        $this->spladeTitle('Transition');
+
+        return view('documentation.transition');
+    }
+
+    /**
+     * Documentation for splade teleport.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function teleport()
+    {
+        $this->spladeTitle('Teleport');
+
+        return view('documentation.teleport');
+    }
+    // ! / END SPLADE COMPONENT DOCUMENTATION ---------------------------------------------------------------
+
+    // ! SPLADE INPUT COMPONENT DOCUMENTATION ---------------------------------------------------------------
+    /**
+     * Documentation for splade input.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function inputOverview()
+    {
+        $this->spladeTitle('Input Overview');
+
+        return view('documentation.input-overview');
+    }
+
+    /**
+     * Documentation for splade input.
+     * 
+     * @return \Illuminate\View\View
      */
     public function input()
     {
+        $this->spladeTitle('Input');
+
         return view('documentation.input');
     }
+
     /**
-     * Show the documentation for input
+     * Documentation for splade textarea.
+     * 
+     * @return \Illuminate\View\View
      */
-    public function controllerHelper()
+    public function textarea()
     {
-        return view('documentation.controller-helper');
+        $this->spladeTitle('Textarea');
+
+        return view('documentation.textarea');
     }
 
     /**
-     * Show the documentation for input
+     * Documentation for splade select.
+     * 
+     * @return \Illuminate\View\View
      */
-    public function uploadFileHelper()
+    public function select()
     {
-        return view('documentation.upload-file-helper');
+        $this->spladeTitle('Select');
+
+        return view('documentation.select');
     }
 
     /**
-     * Show the documentation for input
+     * Documentation for splade checkbox.
+     * 
+     * @return \Illuminate\View\View
      */
-    public function spladeHelper()
+    public function checkbox()
     {
-        return view('documentation.splade-helper');
+        $this->spladeTitle('Checkbox');
+
+        return view('documentation.checkbox');
     }
+
+    /**
+     * Documentation for splade radio.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function radio()
+    {
+        $this->spladeTitle('Radio');
+
+        return view('documentation.radio');
+    }
+
+    /**
+     * Documentation for splade file.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function file()
+    {
+        $this->spladeTitle('File');
+
+        return view('documentation.file');
+    }
+
+    /**
+     * Documentation for splade group.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function group()
+    {
+        $this->spladeTitle('Group');
+
+        return view('documentation.group');
+    }
+
+    /**
+     * Documentation for splade submit.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function submit()
+    {
+        $this->spladeTitle('Submit');
+
+        return view('documentation.submit');
+    }
+
+    /**
+     * Documentation for splade wysiwyg.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function wysiwyg()
+    {
+        $this->spladeTitle('Wysiwyg');
+
+        return view('documentation.wysiwyg');
+    }
+
+    /**
+     * Documentation for splade model binding.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function modelBinding()
+    {
+        $this->spladeTitle('Model Binding');
+
+        return view('documentation.model-binding');
+    }
+
+    // ! / END SPLADE INPUT COMPONENT DOCUMENTATION ---------------------------------------------------------------
+
+    // ! SPLADE TABLE COMPONENT DOCUMENTATION ---------------------------------------------------------------
+    /**
+     * Documentation for splade table.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function tableOverview()
+    {
+        $this->spladeTitle('Table Overview');
+
+        return view('documentation.table-overview');
+    }
+
+    /**
+     * Documentation for splade table Query Builder.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function tableQueryBuilder()
+    {
+        $this->spladeTitle('Table Query Builder');
+
+        return view('documentation.table-query-builder');
+    }
+
+    /**
+     * Documentation for splade table Bulk Action.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function tableBulkAction()
+    {
+        $this->spladeTitle('Table Bulk Action');
+
+        return view('documentation.table-bulk-action');
+    }
+
+    /**
+     * Documentation for splade table Export.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function tableExport()
+    {
+        $this->spladeTitle('Table Export');
+
+        return view('documentation.table-export');
+    }
+
+    /**
+     * Documentation for splade table Spatie Query Builder.
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function tableSpatieQueryBuilder()
+    {
+        $this->spladeTitle('Table Spatie Query Builder');
+
+        return view('documentation.table-spatie-query-builder');
+    }
+    // ! / END SPLADE TABLE COMPONENT DOCUMENTATION ---------------------------------------------------------------
 }
--- routes/documentation.php
+++ routes/documentation.php
@@ -7,12 +7,45 @@ Route::group(['prefix' => 'documentation', 'as' => 'documentation.', 'middleware
 
     Route::get('/', [DocumentationController::class, 'index'])->name('index');
 
-    Route::get('sidebar', [DocumentationController::class, 'sidebar'])->name('sidebar');
-    Route::get('card', [DocumentationController::class, 'card'])->name('card');
-    Route::get('button', [DocumentationController::class, 'button'])->name('button');
-    Route::get('input', [DocumentationController::class, 'input'])->name('input');
+    Route::get('controllerHelper', [DocumentationController::class, 'controllerHelper'])->name('controller-helper');
+    Route::get('uploadFileHelper', [DocumentationController::class, 'uploadFileHelper'])->name('upload-file-helper');
+    Route::get('spladeHelper', [DocumentationController::class, 'spladeHelper'])->name('splade-helper');
 
-    Route::get('controller-helper', [DocumentationController::class, 'controllerHelper'])->name('controller-helper');
-    Route::get('upload-file-helper', [DocumentationController::class, 'uploadFileHelper'])->name('upload-file-helper');
-    Route::get('splade-helper', [DocumentationController::class, 'spladeHelper'])->name('splade-helper');
+    Route::get('sidebar', [DocumentationController::class, 'sidebar'])->name('sidebar');
+    Route::get('config', [DocumentationController::class, 'config'])->name('config');
+
+    Route::get('alert', [DocumentationController::class, 'alert'])->name('alert');
+    Route::get('badge', [DocumentationController::class, 'badge'])->name('badge');
+    Route::get('breadcrumb', [DocumentationController::class, 'breadcrumb'])->name('breadcrumb');
+    Route::get('button', [DocumentationController::class, 'button'])->name('button');
+    Route::get('card', [DocumentationController::class, 'card'])->name('card');
+    Route::get('collapse', [DocumentationController::class, 'collapse'])->name('collapse');
+    Route::get('modal', [DocumentationController::class, 'modal'])->name('modal');
+    Route::get('tooltip', [DocumentationController::class, 'tooltip'])->name('tooltip');
+
+    Route::get('flash', [DocumentationController::class, 'flash'])->name('flash');
+    Route::get('form', [DocumentationController::class, 'form'])->name('form');
+    Route::get('link', [DocumentationController::class, 'link'])->name('link');
+    Route::get('script', [DocumentationController::class, 'script'])->name('script');
+    Route::get('toggle', [DocumentationController::class, 'toggle'])->name('toggle');
+    Route::get('transition', [DocumentationController::class, 'transition'])->name('transition');
+    Route::get('teleport', [DocumentationController::class, 'teleport'])->name('teleport');
+
+    Route::get('inputOverview', [DocumentationController::class, 'inputOverview'])->name('input-overview');
+    Route::get('input', [DocumentationController::class, 'input'])->name('input');
+    Route::get('textarea', [DocumentationController::class, 'textarea'])->name('textarea');
+    Route::get('select', [DocumentationController::class, 'select'])->name('select');
+    Route::get('checkbox', [DocumentationController::class, 'checkbox'])->name('checkbox');
+    Route::get('radio', [DocumentationController::class, 'radio'])->name('radio');
+    Route::get('file', [DocumentationController::class, 'file'])->name('file');
+    Route::get('group', [DocumentationController::class, 'group'])->name('group');
+    Route::get('submit', [DocumentationController::class, 'submit'])->name('submit');
+    Route::get('wysiwyg', [DocumentationController::class, 'wysiwyg'])->name('wysiwyg');
+
+    Route::get('modelBinding', [DocumentationController::class, 'modelBinding'])->name('model-binding');
+    Route::get('tableOverview', [DocumentationController::class, 'tableOverview'])->name('table-overview');
+    Route::get('tableQueryBuilder', [DocumentationController::class, 'tableQueryBuilder'])->name('table-query-builder');
+    Route::get('tableBulkAction', [DocumentationController::class, 'tableBulkAction'])->name('table-bulk-action');
+    Route::get('tableExport', [DocumentationController::class, 'tableExport'])->name('table-export');
+    Route::get('tableSpatieQueryBuilder', [DocumentationController::class, 'tableSpatieQueryBuilder'])->name('table-spatie-query-builder');
 });
