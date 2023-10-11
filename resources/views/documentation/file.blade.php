e44800313c51712099cf2aab977792ba7f093557
Zulfikar Ditya <zulfikarditya.work@gmail.com>
2023-10-11 11:32:33 +0700

    Add: documentation file and radio input splade

--- resources/views/documentation/file.blade.php
+++ resources/views/documentation/file.blade.php
@@ -1,7 +1,35 @@
 <x-app-layout>
 
+    @php
+        $pageName = 'File Input Component';
+    @endphp
+
     <x-breadcrumbs>
-        <x-breadcrumbs-link>Testing Documentation</x-breadcrumbs-link>
+        <x-breadcrumbs-link>Documentation</x-breadcrumbs-link>
+        <x-breadcrumbs-link>{{ $pageName }}</x-breadcrumbs-link>
     </x-breadcrumbs>
+
+    <x-card class="mt-10">
+        <h1 class="card-title">{{ $pageName }}</h1>
+        <hr class="my-3">
+        <p class="text-base-content">
+            This template have build in file input component.
+        </p>
+
+        <div class="mt-5">
+            <div class="mockup-code">
+                <pre data-prefix="">
+                    <code>
+        &lt;x-splade-file name=&quot;avatar&quot; /&gt;
+                    </code>
+                </pre>
+            </div>
+        </div>
+
+        <div class="mt-5">
+            <p>Plase visit <a class="text-secondary" href="https://splade.dev/docs/form-select">Splade documentation</a> for more documentation</p>
+        </div>
+
+    </x-card>
 
 </x-app-layout>
--- resources/views/documentation/radio.blade.php
+++ resources/views/documentation/radio.blade.php
@@ -1,7 +1,38 @@
 <x-app-layout>
 
+    @php
+        $pageName = 'Radio Input Component';
+    @endphp
+
     <x-breadcrumbs>
-        <x-breadcrumbs-link>Testing Documentation</x-breadcrumbs-link>
+        <x-breadcrumbs-link>Documentation</x-breadcrumbs-link>
+        <x-breadcrumbs-link>{{ $pageName }}</x-breadcrumbs-link>
     </x-breadcrumbs>
+
+    <x-card class="mt-10">
+        <h1 class="card-title">{{ $pageName }}</h1>
+        <hr class="my-3">
+        <p class="text-base-content">
+            This template have build in radio input component.
+        </p>
+
+        <div class="mt-5">
+            <div class="mockup-code">
+                <pre data-prefix="">
+                    <code>
+    &lt;x-splade-group name=&quot;notification_channel&quot; label=&quot;Preferred notification channel&quot; inline&gt;
+        &lt;x-splade-radio name=&quot;notification_channel&quot; value=&quot;mail&quot; label=&quot;Mail&quot; /&gt;
+        &lt;x-splade-radio name=&quot;notification_channel&quot; value=&quot;slack&quot; label=&quot;Slack&quot; /&gt;
+    &lt;/x-splade-group&gt;
+                    </code>
+                </pre>
+            </div>
+        </div>
+
+        <div class="mt-5">
+            <p>Plase visit <a class="text-secondary" href="https://splade.dev/docs/form-radio">Splade documentation</a> for more documentation</p>
+        </div>
+
+    </x-card>
 
 </x-app-layout>
