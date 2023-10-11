2666400ab50a64fc52c7697ad811b247db43f631
Zulfikar Ditya <zulfikarditya.work@gmail.com>
2023-10-11 10:35:44 +0700

    Add: select and checkbox splade component

--- resources/views/documentation/checkbox.blade.php
+++ resources/views/documentation/checkbox.blade.php
@@ -1,7 +1,35 @@
 <x-app-layout>
 
+    @php
+        $pageName = 'Checkboc Input Component';
+    @endphp
+
     <x-breadcrumbs>
-        <x-breadcrumbs-link>Testing Documentation</x-breadcrumbs-link>
+        <x-breadcrumbs-link>Documentation</x-breadcrumbs-link>
+        <x-breadcrumbs-link>{{ $pageName }}</x-breadcrumbs-link>
     </x-breadcrumbs>
+
+    <x-card class="mt-10">
+        <h1 class="card-title">{{ $pageName }}</h1>
+        <hr class="my-3">
+        <p class="text-base-content">
+            This template have build in Checkboc Input Component.
+        </p>
+
+        <div class="mt-5">
+            <div class="mockup-code">
+                <pre data-prefix="">
+                    <code>
+    &lt;x-splade-checkbox name=&quot;newsletter&quot; value=&quot;1&quot; label=&quot;Subscribe to newsletter&quot; /&gt;
+                    </code>
+                </pre>
+            </div>
+        </div>
+
+        <div class="mt-5">
+            <p>Plase visit <a class="text-secondary" href="https://splade.dev/docs/form-checkbox">Splade documentation</a> for more documentation</p>
+        </div>
+
+    </x-card>
 
 </x-app-layout>
