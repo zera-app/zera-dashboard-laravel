ea62a7edebad5855587e5b2bb7e19bb9f0fd7fce
Zulfikar Ditya <52193940+zulfikar-ditya@users.noreply.github.com>
2024-10-06 20:18:45 +0700

    WIP

--- resources/views/documentation/table-overview.blade.php
+++ resources/views/documentation/table-overview.blade.php
@@ -1,7 +1,18 @@
 <x-app-layout>
 
+    @php
+        $pageName = 'Table Overview';
+    @endphp
+
     <x-breadcrumbs>
-        <x-breadcrumbs-link>Testing Documentation</x-breadcrumbs-link>
+        <x-breadcrumbs-link>Table Component</x-breadcrumbs-link>
+        <x-breadcrumbs-link>{{ $pageName }}</x-breadcrumbs-link>
     </x-breadcrumbs>
+
+    <x-card class="mt-10" body-class="">
+        <h1 class="card-title">{{ $pageName }}</h1>
+        <hr class="my-2">
+        <x-splade-table :for="$data" />
+    </x-card>
 
 </x-app-layout>
